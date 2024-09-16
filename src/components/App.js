a2a741491fc530a41d282097c48bd8beedb15a7a
yokas <158809259+yokiedenis@users.noreply.github.com>
2024-09-16 16:12:08 +0000

    fistr

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,9 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+const App=()=>{
+    <div id="main">
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+}
+
+export default App;
+
 
